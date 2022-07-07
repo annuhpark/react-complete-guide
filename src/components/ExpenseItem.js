4a56bb065ad5db75524c1a25acaf913b94f186d5
Anna Park <parkannuh@gmail.com>
2022-07-07 11:19:25 -0700

    Added white text to description & price headings.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-  return (<>
-    <div>
+  return (
+  <div className="expense-item">
+    <div className="expense-item__description">
       March 28th, 2021
     </div>
-    <div>
+    <div className="expense-item__description">
       Car Insurance
     </div>
-    <div>
+    <div className="expense-item__price">
       $294.67
     </div>
-  </>
+  </div>
   );
 };
 
