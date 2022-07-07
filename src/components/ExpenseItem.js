112ca0af9459e6481445aba5fe1e636687028639
Anna Park <parkannuh@gmail.com>
2022-07-07 11:44:54 -0700

    Removed hard-coding.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
   return (
   <div className="expense-item">
+    <div>{expenseDate.toISOString()}</div>
     <div className="expense-item__description">
-      March 28th, 2021
-    </div>
-    <div className="expense-item__description">
-      Car Insurance
-    </div>
-    <div className="expense-item__price">
-      $294.67
+      <h2>{expenseTitle}</h2>
+      <div className="expense-item__price">
+        ${expenseAmount}
+      </div>
     </div>
   </div>
   );
